0ce8dd65c7a0a025ea33a346ea58f82a3aab5bfc
Made Baihaqi <90297940+f2ezeee@users.noreply.github.com>
2021-10-06 11:33:42 +0700

    ehe2

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/buku', function () {
+    return view('buku');
+});
+
+Route::get('/pengajar', function () {
+    return view('pengajar');
+});
+
+Route::get('/santri', function () {
+    return view('santri');
+});
